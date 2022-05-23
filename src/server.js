569532c21fb7c7e5be144656ef820439632379fb
manishbawa0001 <manishbawa940@gmail.com>
2022-05-24 00:30:45 +0530

    <html-include src=my-html.html></html-include>

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello, Welcome to DevOps CI CD, we are Manish and Naman This side");
+    response.end( <html-include src="my-html.html"></html-include> );
 
 });
 
